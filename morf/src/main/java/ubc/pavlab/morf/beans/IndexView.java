31a9b931f6b6f77b1f4ff054e72b0b36b081ad47
JacobsonMT <JacobsonMT+github@gmail.com>
2015-03-23 21:34:36 -0700

    Fixed bug: input file should always have newline character at end

--- morf/src/main/java/ubc/pavlab/morf/beans/IndexView.java
+++ morf/src/main/java/ubc/pavlab/morf/beans/IndexView.java
@@ -18,130 +18,130 @@ import ubc.pavlab.morf.models.Job;
 @ViewScoped
 public class IndexView implements Serializable {
 
-    /**
+	/**
 	 * 
 	 */
-    private static final long serialVersionUID = 2438909388677798292L;
+	private static final long serialVersionUID = 2438909388677798292L;
 
-    private static final Logger log = Logger.getLogger( IndexView.class );
+	private static final Logger log = Logger.getLogger(IndexView.class);
 
-    @ManagedProperty(value = "#{userManager}")
-    private UserManager userManager;
+	@ManagedProperty(value = "#{userManager}")
+	private UserManager userManager;
 
-    // private String currentSelectedName;
-    private String name;
-    private String content;
-    private Job selectedJob;
-    private Job jobToRemove;
-    private Integer exampleCnt = 1;
+	// private String currentSelectedName;
+	private String name;
+	private String content;
+	private Job selectedJob;
+	private Job jobToRemove;
+	private Integer exampleCnt = 1;
 
-    public IndexView() {
-        log.info( "IndexView created" );
-    }
+	public IndexView() {
+		log.info("IndexView created");
+	}
 
-    @PostConstruct
-    public void init() {
-        log.info( "IndexView init" );
-    }
+	@PostConstruct
+	public void init() {
+		log.info("IndexView init");
+	}
 
-    /*
-     * public void getResult(ActionEvent actionEvent) { String res = userManager.getResultIfReady(currentSelectedName);
-     * if (res != null) { addMessage(res); } else { addMessage("Something went wrong!"); } }
-     */
+	/*
+	 * public void getResult(ActionEvent actionEvent) { String res = userManager.getResultIfReady(currentSelectedName);
+	 * if (res != null) { addMessage(res); } else { addMessage("Something went wrong!"); } }
+	 */
 
-    public void cancelJob() {
+	public void cancelJob() {
 
-        if ( jobToRemove.getComplete() ) {
-            addMessage( "Job (" + jobToRemove.getName() + ") is already complete, cannot cancel.",
-                    FacesMessage.SEVERITY_INFO );
-        } else if ( userManager.jobExists( jobToRemove ) ) {
-            boolean canceled = userManager.cancelJob( jobToRemove );
-            if ( canceled ) {
-                addMessage( "Job (" + jobToRemove.getName() + ") successfully cancelled.", FacesMessage.SEVERITY_INFO );
-            } else {
-                addMessage( "Job (" + jobToRemove.getName() + ") failed to cancel.", FacesMessage.SEVERITY_WARN );
-            }
-        } else {
-            addMessage( "Cannot find job (" + jobToRemove.getName() + ")", FacesMessage.SEVERITY_ERROR );
+		if (jobToRemove.getComplete()) {
+			addMessage("Job (" + jobToRemove.getName() + ") is already complete, cannot cancel.",
+					FacesMessage.SEVERITY_INFO);
+		} else if (userManager.jobExists(jobToRemove)) {
+			boolean canceled = userManager.cancelJob(jobToRemove);
+			if (canceled) {
+				addMessage("Job (" + jobToRemove.getName() + ") successfully cancelled.", FacesMessage.SEVERITY_INFO);
+			} else {
+				addMessage("Job (" + jobToRemove.getName() + ") failed to cancel.", FacesMessage.SEVERITY_WARN);
+			}
+		} else {
+			addMessage("Cannot find job (" + jobToRemove.getName() + ")", FacesMessage.SEVERITY_ERROR);
 
-        }
-    }
+		}
+	}
 
-    public void submitJob( ActionEvent actionEvent ) {
+	public void submitJob(ActionEvent actionEvent) {
 
-        Job job = new Job( userManager.getSessionId(), name, content );
-        if ( userManager.jobExists( job ) ) {
-            addMessage( "Job already exists under name (" + name + ")", FacesMessage.SEVERITY_WARN );
-        } else {
-            addMessage( "Job submitted for (" + name + ")", FacesMessage.SEVERITY_INFO );
-            userManager.submitJob( job );
-            // RequestContext.getCurrentInstance().addCallbackParam("stopPolling", false);
-        }
+		Job job = new Job(userManager.getSessionId(), name, content);
+		if (userManager.jobExists(job)) {
+			addMessage("Job already exists under name (" + name + ")", FacesMessage.SEVERITY_WARN);
+		} else {
+			addMessage("Job submitted for (" + name + ")", FacesMessage.SEVERITY_INFO);
+			userManager.submitJob(job);
+			// RequestContext.getCurrentInstance().addCallbackParam("stopPolling", false);
+		}
 
-    }
+	}
 
-    public void submitMultipleJob( ActionEvent actionEvent ) {
-        for ( int i = 0; i < 5; i++ ) {
-            Job job = new Job( userManager.getSessionId(), exampleCnt.toString(), exampleCnt.toString() );
-            if ( userManager.jobExists( job ) ) {
-                addMessage( "Job already exists under name (" + exampleCnt.toString() + ")", FacesMessage.SEVERITY_WARN );
-                exampleCnt++;
-            } else {
-                addMessage( "Job submitted for (" + exampleCnt.toString() + ")", FacesMessage.SEVERITY_INFO );
-                userManager.submitJob( job );
-                exampleCnt++;
-            }
+	public void submitMultipleJob(ActionEvent actionEvent) {
+		for (int i = 0; i < 5; i++) {
+			Job job = new Job(userManager.getSessionId(), exampleCnt.toString(), exampleCnt.toString());
+			if (userManager.jobExists(job)) {
+				addMessage("Job already exists under name (" + exampleCnt.toString() + ")", FacesMessage.SEVERITY_WARN);
+				exampleCnt++;
+			} else {
+				addMessage("Job submitted for (" + exampleCnt.toString() + ")", FacesMessage.SEVERITY_INFO);
+				userManager.submitJob(job);
+				exampleCnt++;
+			}
 
-        }
+		}
 
-    }
+	}
 
-    private void addMessage( String summary, FacesMessage.Severity severity ) {
-        FacesMessage message = new FacesMessage( severity, summary, null );
-        FacesContext.getCurrentInstance().addMessage( null, message );
-    }
+	private void addMessage(String summary, FacesMessage.Severity severity) {
+		FacesMessage message = new FacesMessage(severity, summary, null);
+		FacesContext.getCurrentInstance().addMessage(null, message);
+	}
 
-    /*
-     * public String getCurrentSelectedName() { return currentSelectedName; }
-     * 
-     * public void setCurrentSelectedName(String currentSelectedName) { this.currentSelectedName = currentSelectedName;
-     * }
-     */
+	/*
+	 * public String getCurrentSelectedName() { return currentSelectedName; }
+	 * 
+	 * public void setCurrentSelectedName(String currentSelectedName) { this.currentSelectedName = currentSelectedName;
+	 * }
+	 */
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName( String name ) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public String getContent() {
-        return content;
-    }
+	public String getContent() {
+		return content;
+	}
 
-    public void setContent( String content ) {
-        this.content = content;
-    }
+	public void setContent(String content) {
+		this.content = content.trim() + "\r\n";
+	}
 
-    public void setUserManager( UserManager userManager ) {
-        this.userManager = userManager;
-    }
+	public void setUserManager(UserManager userManager) {
+		this.userManager = userManager;
+	}
 
-    public Job getSelectedJob() {
-        return selectedJob;
-    }
+	public Job getSelectedJob() {
+		return selectedJob;
+	}
 
-    public void setSelectedJob( Job selectedJob ) {
-        this.selectedJob = selectedJob;
-    }
+	public void setSelectedJob(Job selectedJob) {
+		this.selectedJob = selectedJob;
+	}
 
-    public Job getJobToRemove() {
-        return jobToRemove;
-    }
+	public Job getJobToRemove() {
+		return jobToRemove;
+	}
 
-    public void setJobToRemove( Job jobToRemove ) {
-        this.jobToRemove = jobToRemove;
-    }
+	public void setJobToRemove(Job jobToRemove) {
+		this.jobToRemove = jobToRemove;
+	}
 
 }
--- morf/src/main/java/ubc/pavlab/morf/models/Job.java
+++ morf/src/main/java/ubc/pavlab/morf/models/Job.java
@@ -20,12 +20,14 @@
 package ubc.pavlab.morf.models;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.io.StringWriter;
 import java.util.Date;
 import java.util.concurrent.Callable;
@@ -47,227 +49,231 @@ import org.primefaces.model.StreamedContent;
  */
 public class Job implements Callable<String> {
 
-    private static final Logger log = Logger.getLogger( Job.class );
-    private static String scriptName;
-    private static String scriptBasePath;
-    private static String pathToInput;
-    private static String pathToOutput;
+	private static final Logger log = Logger.getLogger(Job.class);
+	private static String scriptName;
+	private static String scriptBasePath;
+	private static String pathToInput;
+	private static String pathToOutput;
 
-    private String sessionId;
-    private String name;
-    private String content;
-    private Boolean complete = false;
-    private Future<String> future;
-    // private String result;
-    private StreamedContent resultFile;
-    private Date submittedDate;
-    private String position;
+	private String sessionId;
+	private String name;
+	private String content;
+	private Boolean complete = false;
+	private Future<String> future;
+	// private String result;
+	private StreamedContent resultFile;
+	private Date submittedDate;
+	private String position;
 
-    public Job() {
+	public Job() {
 
-    }
+	}
 
-    /**
-     * @param sessionId
-     * @param name
-     * @param contents
-     */
-    public Job( String sessionId, String name, String content ) {
-        super();
-        this.sessionId = sessionId;
-        this.name = name;
-        this.content = content;
-    }
+	/**
+	 * @param sessionId
+	 * @param name
+	 * @param contents
+	 */
+	public Job(String sessionId, String name, String content) {
+		super();
+		this.sessionId = sessionId;
+		this.name = name;
+		this.content = content;
+	}
 
-    public String getSessionId() {
-        return sessionId;
-    }
+	public String getSessionId() {
+		return sessionId;
+	}
 
-    public void setSessionId( String sessionId ) {
-        this.sessionId = sessionId;
-    }
+	public void setSessionId(String sessionId) {
+		this.sessionId = sessionId;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName( String name ) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public String getContent() {
-        return content;
-    }
+	public String getContent() {
+		return content;
+	}
 
-    public void setContent( String content ) {
-        this.content = content;
-    }
+	public void setContent(String content) {
+		this.content = content;
+	}
 
-    public Future<String> getFuture() {
-        return future;
-    }
+	public Future<String> getFuture() {
+		return future;
+	}
 
-    public void setFuture( Future<String> future ) {
-        this.future = future;
-    }
+	public void setFuture(Future<String> future) {
+		this.future = future;
+	}
 
-    public Boolean getComplete() {
-        if ( complete ) {
-            return true;
-        } else {
-            if ( this.future == null ) {
-                return false;
-            } else {
-                return this.future.isDone();
-            }
-        }
-    }
+	public Boolean getComplete() {
+		if (complete) {
+			return true;
+		} else {
+			if (this.future == null) {
+				return false;
+			} else {
+				return this.future.isDone();
+			}
+		}
+	}
 
-    public void setComplete( Boolean complete ) {
-        this.complete = complete;
-    }
+	public void setComplete(Boolean complete) {
+		this.complete = complete;
+	}
 
-    // public String getResult() {
-    // if ( result != null ) {
-    // return result;
-    // }
-    // if ( getComplete() ) {
-    // try {
-    // this.result = this.future.get( 1, TimeUnit.SECONDS );
-    // return result;
-    // } catch ( InterruptedException | ExecutionException | TimeoutException e ) {
-    // e.printStackTrace();
-    // return null;
-    // }
-    // } else {
-    // return null;
-    // }
-    //
-    // }
+	// public String getResult() {
+	// if ( result != null ) {
+	// return result;
+	// }
+	// if ( getComplete() ) {
+	// try {
+	// this.result = this.future.get( 1, TimeUnit.SECONDS );
+	// return result;
+	// } catch ( InterruptedException | ExecutionException | TimeoutException e ) {
+	// e.printStackTrace();
+	// return null;
+	// }
+	// } else {
+	// return null;
+	// }
+	//
+	// }
 
-    public StreamedContent getFile() {
-        if ( getComplete() ) {
-            try {
-                InputStream in = IOUtils.toInputStream( this.future.get( 1, TimeUnit.SECONDS ), "UTF-8" );
-                this.resultFile = new DefaultStreamedContent( in, "text/plain", this.name + ".txt" );
-                return resultFile;
-            } catch ( InterruptedException | ExecutionException | TimeoutException | IOException e ) {
-                e.printStackTrace();
-                return null;
-            }
-        } else {
-            return null;
-        }
-    }
+	public StreamedContent getFile() {
+		if (getComplete()) {
+			try {
+				InputStream in = IOUtils.toInputStream(this.future.get(1, TimeUnit.SECONDS), "UTF-8");
+				this.resultFile = new DefaultStreamedContent(in, "text/plain", this.name + ".txt");
+				return resultFile;
+			} catch (InterruptedException | ExecutionException | TimeoutException | IOException e) {
+				e.printStackTrace();
+				return null;
+			}
+		} else {
+			return null;
+		}
+	}
 
-    @Override
-    public String toString() {
-        return "Job [sessionId=" + sessionId + ", name=" + name + ", complete=" + complete + "]";
-    }
+	@Override
+	public String toString() {
+		return "Job [sessionId=" + sessionId + ", name=" + name + ", complete=" + complete + "]";
+	}
 
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ( ( name == null ) ? 0 : name.hashCode() );
-        result = prime * result + ( ( sessionId == null ) ? 0 : sessionId.hashCode() );
-        return result;
-    }
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((sessionId == null) ? 0 : sessionId.hashCode());
+		return result;
+	}
 
-    @Override
-    public boolean equals( Object obj ) {
-        if ( this == obj ) return true;
-        if ( obj == null ) return false;
-        if ( getClass() != obj.getClass() ) return false;
-        Job other = ( Job ) obj;
-        if ( name == null ) {
-            if ( other.name != null ) return false;
-        } else if ( !name.equals( other.name ) ) return false;
-        if ( sessionId == null ) {
-            if ( other.sessionId != null ) return false;
-        } else if ( !sessionId.equals( other.sessionId ) ) return false;
-        return true;
-    }
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Job other = (Job) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (sessionId == null) {
+			if (other.sessionId != null)
+				return false;
+		} else if (!sessionId.equals(other.sessionId))
+			return false;
+		return true;
+	}
 
-    @Override
-    public String call() throws Exception {
-        log.info( "Starting job " + name + " for session: " + sessionId );
-        // Write content to input
-        File file = new File( pathToInput );
+	@Override
+	public String call() throws Exception {
+		log.info("Starting job " + name + " for session: " + sessionId);
+		// Write content to input
+		File file = new File(pathToInput);
 
-        try (FileOutputStream fop = new FileOutputStream( file )) {
+		try (FileOutputStream fop = new FileOutputStream(file)) {
 
-            // if file doesn't exists, then create it
-            if ( !file.exists() ) {
-                file.createNewFile();
-            }
+			// if file doesn't exists, then create it
+			if (!file.exists()) {
+				file.createNewFile();
+			}
 
-            // get the content in bytes
-            byte[] contentInBytes = content.getBytes();
+			BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(fop));
+			writer.write(content);
+			writer.close();
 
-            fop.write( contentInBytes );
-            fop.flush();
-            fop.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
-        } catch ( IOException e ) {
-            e.printStackTrace();
-        }
+		// Execute script
+		executeCommand("./" + scriptName, scriptBasePath);
 
-        // Execute script
-        executeCommand( "./" + scriptName, scriptBasePath );
+		// Get output
+		InputStream resultFile = new FileInputStream(pathToOutput);
 
-        // Get output
-        InputStream resultFile = new FileInputStream( pathToOutput );
+		StringWriter writer = new StringWriter();
+		IOUtils.copy(resultFile, writer, "UTF-8");
+		log.info("Finished job " + name + " for session: " + sessionId);
+		return writer.toString();
+	}
 
-        StringWriter writer = new StringWriter();
-        IOUtils.copy( resultFile, writer, "UTF-8" );
-        log.info( "Finished job " + name + " for session: " + sessionId );
-        return writer.toString();
-    }
+	private String executeCommand(String command, String path) {
 
-    private String executeCommand( String command, String path ) {
+		StringBuffer output = new StringBuffer();
 
-        StringBuffer output = new StringBuffer();
+		Process p;
+		try {
+			p = Runtime.getRuntime().exec(command, null, new File(path));
+			p.waitFor();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
 
-        Process p;
-        try {
-            p = Runtime.getRuntime().exec( command, null, new File( path ) );
-            p.waitFor();
-            BufferedReader reader = new BufferedReader( new InputStreamReader( p.getInputStream() ) );
+			String line = "";
+			while ((line = reader.readLine()) != null) {
+				output.append(line + "\n");
+			}
 
-            String line = "";
-            while ( ( line = reader.readLine() ) != null ) {
-                output.append( line + "\n" );
-            }
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
-        } catch ( Exception e ) {
-            e.printStackTrace();
-        }
+		return output.toString();
+	}
 
-        return output.toString();
-    }
+	public Date getSubmittedDate() {
+		return submittedDate;
+	}
 
-    public Date getSubmittedDate() {
-        return submittedDate;
-    }
+	public void setSubmittedDate(Date submittedDate) {
+		this.submittedDate = submittedDate;
+	}
 
-    public void setSubmittedDate( Date submittedDate ) {
-        this.submittedDate = submittedDate;
-    }
+	public String getPosition() {
+		return position;
+	}
 
-    public String getPosition() {
-        return position;
-    }
+	public void setPosition(String position) {
+		this.position = position;
+	}
 
-    public void setPosition( String position ) {
-        this.position = position;
-    }
-
-    public static void setPaths( String scriptName, String scriptBasePath, String pathToInput, String pathToOutput ) {
-        Job.scriptName = scriptName;
-        Job.scriptBasePath = scriptBasePath;
-        Job.pathToInput = pathToInput;
-        Job.pathToOutput = pathToOutput;
-    }
+	public static void setPaths(String scriptName, String scriptBasePath, String pathToInput, String pathToOutput) {
+		Job.scriptName = scriptName;
+		Job.scriptBasePath = scriptBasePath;
+		Job.pathToInput = pathToInput;
+		Job.pathToOutput = pathToOutput;
+	}
 
 }
